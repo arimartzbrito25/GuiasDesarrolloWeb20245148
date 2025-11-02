bc4df3dbc4fc926ca098bc32eddedb00d3bb4598
arimartzbrito25 <arimartzbrito25@gmail.com>
2025-11-01 20:50:06 -0600

    Add JavaScript fundamentals guide and examples
    Added Guia5_20245148 directory with HTML, CSS, and JS files demonstrating basic JavaScript concepts: alerts, prompts, DOM manipulation, table creation, random number guessing, and arithmetic operations. Updated main index.html to link to new guide and examples.

---
+++ Guia5_20245148/js/buttons.js
@@ -0,0 +1,26 @@
+function aviso(){
+    alert("Bienvenido al mundo JavaScript")
+}
+
+function confirmacion(){
+    //Los  valores que puede almacenar la variable confirmacion
+    // son true o false
+    let confirmacion = confirm("¿Desea salir de la Sesión");
+    alert (`Valor seleccionado ${confirmacion}`);
+}
+
+function capturarDatos(){
+    let nombre = prompt("¿Cuál es su nombre?");
+    let edad = prompt("¿Cuál es su edad?",0);
+
+    alert(`Su nombre es ${nombre} y su edad ${edad}`);
+}
+
+function dibujarParrafo(){
+    let parrafo = prompt(
+        "Escriba la información que desea visualizar en el parrafo"
+    ); 
+
+    const p = document.querySelector("#idParrafo");
+    p.innerHTML = parrafo; 
+}
